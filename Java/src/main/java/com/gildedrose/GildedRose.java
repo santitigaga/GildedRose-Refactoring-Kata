ceffd97f26a89170ee6cc7dcf3ffeb8b1271c5bf
Santiago Garcia <santitigaga@hotmail.com>
2020-09-30 17:16:07 -0500

    refactor by item

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,6 @@
 package com.gildedrose;
 
 class GildedRose {
-    private static final int MAX_DEFAULT_QUALITY = 50;
     private static final String BACKSTAGE_ITEM = "Backstage passes to a TAFKAL80ETC concert";
     private static final String CONJURED_ITEM = "Conjured Mana Cake";
     private static final String AGED_ITEM = "Aged Brie";
@@ -14,77 +13,68 @@ class GildedRose {
     }
 
     public void updateQuality() {
+
         for (Item item : items) {
             reduceSell(item);
-            if (isEspecial(item)) {
-                value(item);
-            } else {
-                devalue(item);
-            }
-            if (item.sellIn < 0) {
-                alterQualityWhenDaysAreOver(item);
+            switch (item.name) {
+                case BACKSTAGE_ITEM:
+                    changeQualityBackstage(item);
+                    break;
+                case SULFURAS_ITEM:
+                    changeQualitySulfuras(item);
+                    break;
+                case AGED_ITEM:
+                    changeQualityAged(item);
+                    break;
+                case CONJURED_ITEM:
+                    changeQualityConjured(item);
+                    break;
+                default:
+                    changeQualityDefault(item);
+                    break;
             }
         }
     }
+
     private void reduceSell(Item item) {
         if (!item.name.equals(SULFURAS_ITEM)) {
-            item.sellIn --;
+            item.sellIn--;
         }
     }
 
-    private boolean isEspecial(Item item) {
-        return item.name.equals(AGED_ITEM)
-                || item.name.equals(BACKSTAGE_ITEM) || item.name.equals(SULFURAS_ITEM);
-    }
-
-    private void value(Item item) {
-        if (canIncreaseQuality(item.quality)) {
-            if (item.name.equals(BACKSTAGE_ITEM)) {
-                increaseValueBackstage(item);
-            } else {
-                increaseQuality(item);
-            }
+    public void changeQualityBackstage(Item item) {
+        item.quality++;
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality++;
         }
-    }
-    private boolean canIncreaseQuality(int quality) {
-        return quality < MAX_DEFAULT_QUALITY;
-    }
-    private void increaseValueBackstage(Item item) {
-        increaseQuality(item);
-        if (item.sellIn < 11 && canIncreaseQuality(item.quality)) {
-            increaseQuality(item);
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality++;
         }
-        if (item.sellIn < 6 && canIncreaseQuality(item.quality)) {
-            increaseQuality(item);
-        }
-    }
-    private void increaseQuality(Item item) {
-        item.quality ++;
-    }
-
-    private void devalue(Item item) {
-        if (item.name.equals(CONJURED_ITEM)) {
-            reduceQuality(item, 2);
-        } else {
-            reduceQuality(item, 1);
-        }
-    }
-    private void reduceQuality(Item item, int decrement) {
-        if (item.quality > 0) {
-            item.quality = item.quality - decrement;
-        }
-    }
-
-    private void alterQualityWhenDaysAreOver(Item item) {
-        if (item.name.equals(AGED_ITEM) && canIncreaseQuality(item.quality)) {
-            increaseQuality(item);
-        } else if (item.name.equals(BACKSTAGE_ITEM)) {
+        if (item.sellIn < 0) {
             item.quality = 0;
-        } else {
-            if (item.quality > 0 && !item.name.equals(SULFURAS_ITEM)) {
-                reduceQuality(item, 1);
-            }
         }
+    }
+
+    public void changeQualityAged(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    public void changeQualityConjured(Item item) {
+        if (item.quality > 2) {
+            item.quality = item.quality - 2;
+        } else if (item.quality == 1) {
+            item.quality--;
+        }
+    }
+
+    public void changeQualitySulfuras(Item item) {
+        item.quality = 80;
+    }
+
+    public void changeQualityDefault(Item item) {
+        item.quality--;
     }
 
 }
